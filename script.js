69811a64c4ddfb34f6f7f8412785e10752a40ca8
Camila Pereira <camila379.cp@gmail.com>
2020-12-25 17:15:48 -0300

    Estilizando o body

--- script.js
+++ script.js
@@ -48,6 +48,14 @@ function update (event){
 }
 
 function iniciarJogo(){
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('GAME OVER :(');
+        }
+    }
+
     if(snake[0].x > 15 * box && direction == "right") {
         snake[0].x = 0;
     }
